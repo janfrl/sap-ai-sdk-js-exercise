02cfce6c2f9874424d564efb41299f83b8d41ca0
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2025-06-11 11:19:11 +0200

    fix: use name property to retrieve exercise config

--- server/utils/configs/index.ts
+++ server/utils/configs/index.ts
@@ -6,6 +6,6 @@ export { type ExerciseConfig } from './config-types'
 
 export const configs = [ex1, ex2, ex3]
 
-export function getExerciseConfig(id: string) {
-  return configs.find(c => c.id === id) || ex1
+export function getExerciseConfig(name: string) {
+  return configs.find(c => c.name === name) || ex1
 }
