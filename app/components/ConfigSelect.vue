36e01b3d6c86b70abd305200eebea7d27f0f8899
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2025-06-11 10:46:30 +0200

    feat: add minimum width to SelectMenus

--- app/components/ConfigSelect.vue
+++ app/components/ConfigSelect.vue
@@ -5,9 +5,9 @@ const { config, configs } = useExerciseConfig()
 <template>
   <USelectMenu
     v-model="config"
-    :items="configs as any"
+    :items="configs"
     variant="ghost"
-    class="hover:bg-default focus:bg-default data-[state=open]:bg-default"
+    class="min-w-48 hover:bg-default focus:bg-default data-[state=open]:bg-default"
     :ui="{ trailingIcon: 'group-data-[state=open]:rotate-180 transition-transform duration-200' }"
   />
 </template>
